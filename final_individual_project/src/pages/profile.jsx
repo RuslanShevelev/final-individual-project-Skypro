0232ce626140256fede08ec2bd0bc4a8cb378a98
RuslanShevelev <shevelevrus@gmail.com>
2023-12-14 16:49:22 +0300

    seller-profile

--- final_individual_project/src/pages/profile.jsx
+++ final_individual_project/src/pages/profile.jsx
@@ -4,16 +4,20 @@ import classNames from 'classnames'
 import { Card } from 'components/card/card'
 import { MyButton } from 'components/button/button'
 
-export const Profile = () => {
+export const Profile = ({ myProfile }) => {
   return (
     <div className={styles.main__container}>
       <div className={styles.main__centerBlock}>
-        <h2 className={styles.main__h2}>Здравствуйте, Антон!</h2>
+        <h2 className={styles.main__h2}>
+          {myProfile ? 'Здравствуйте, Антон!' : 'Профиль продавца'}
+        </h2>
         <div className={classNames(styles.main__profile, styles.profile)}>
           <div className={styles.profile__content}>
-            <h3 className={classNames(styles.profile__title, styles.title)}>
-              Настройки профиля
-            </h3>
+            {myProfile && (
+              <h3 className={classNames(styles.profile__title, styles.title)}>
+                Настройки профиля
+              </h3>
+            )}
             <div
               className={classNames(styles.profile__settings, styles.settings)}
             >
@@ -23,64 +27,85 @@ export const Profile = () => {
                     <img src="#" alt="" />
                   </a>
                 </div>
-                <a
-                  className={styles.settings__changePhoto}
-                  href=""
-                  target="_self"
-                >
-                  Заменить
-                </a>
+                {myProfile && (
+                  <a
+                    className={styles.settings__changePhoto}
+                    href=""
+                    target="_self"
+                  >
+                    Заменить
+                  </a>
+                )}
               </div>
               <div className={styles.settings__right}>
-                <form className={styles.settings__form} action="#">
-                  <div className={styles.settings__div}>
-                    <label htmlFor="fname">Имя</label>
-                    <input
-                      className={styles.settings__fName}
-                      id="settings-fname"
-                      name="fname"
-                      type="text"
-                      defaultValue="Антон"
-                      placeholder=""
-                    />
+                {myProfile ? (
+                  <form className={styles.settings__form} action="#">
+                    <div className={styles.settings__div}>
+                      <label htmlFor="fname">Имя</label>
+                      <input
+                        className={styles.settings__fName}
+                        id="settings-fname"
+                        name="fname"
+                        type="text"
+                        defaultValue="Антон"
+                        placeholder=""
+                      />
+                    </div>
+                    <div className={styles.settings__div}>
+                      <label htmlFor="lname">Фамилия</label>
+                      <input
+                        className={styles.settings__lName}
+                        id="settings-lname"
+                        name="lname"
+                        type="text"
+                        defaultValue="Городецкий"
+                        placeholder=""
+                      />
+                    </div>
+                    <div className={styles.settings__div}>
+                      <label htmlFor="city">Город</label>
+                      <input
+                        className={styles.settings__city}
+                        id="settings-city"
+                        name="city"
+                        type="text"
+                        defaultValue="Санкт-Петербург"
+                        placeholder=""
+                      />
+                    </div>
+                    <div className={styles.settings__div}>
+                      <label htmlFor="phone">Телефон</label>
+                      <input
+                        className={styles.settings__phone}
+                        id="settings-phone"
+                        name="phone"
+                        type="tel"
+                        defaultValue={89161234567}
+                        placeholder={+79161234567}
+                      />
+                    </div>
+                    <div className={styles.settings__btn}>
+                      <MyButton name="Сохранить" />
+                    </div>
+                  </form>
+                ) : (
+                  <div className="seller__right">
+                    <h3 className="seller__title">Кирилл Матвеев</h3>
+                    <p className="seller__city">Санкт-Петербург</p>
+                    <p className="seller__inf">Продает товары с августа 2021</p>
+                    <div className="seller__img-mob-block">
+                      <div className="seller__img-mob">
+                        <a href="" target="_self">
+                          <img src="#" alt="" />
+                        </a>
+                      </div>
+                    </div>
+                    <button className="seller__btn btn-hov02">
+                      Показать&nbsp;телефон
+                      <span>8&nbsp;905&nbsp;ХХХ&nbsp;ХХ&nbsp;ХХ</span>
+                    </button>
                   </div>
-                  <div className={styles.settings__div}>
-                    <label htmlFor="lname">Фамилия</label>
-                    <input
-                      className={styles.settings__lName}
-                      id="settings-lname"
-                      name="lname"
-                      type="text"
-                      defaultValue="Городецкий"
-                      placeholder=""
-                    />
-                  </div>
-                  <div className={styles.settings__div}>
-                    <label htmlFor="city">Город</label>
-                    <input
-                      className={styles.settings__city}
-                      id="settings-city"
-                      name="city"
-                      type="text"
-                      defaultValue="Санкт-Петербург"
-                      placeholder=""
-                    />
-                  </div>
-                  <div className={styles.settings__div}>
-                    <label htmlFor="phone">Телефон</label>
-                    <input
-                      className={styles.settings__phone}
-                      id="settings-phone"
-                      name="phone"
-                      type="tel"
-                      defaultValue={89161234567}
-                      placeholder={+79161234567}
-                    />
-                  </div>
-                  <div className={styles.settings__btn}>
-                    <MyButton name="Сохранить" />
-                  </div>
-                </form>
+                )}
               </div>
             </div>
           </div>
